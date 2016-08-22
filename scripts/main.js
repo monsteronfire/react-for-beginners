8f912691726283f3e36bcc43cf5538d9c05c2376
monsteronfire <monsteronfire@gmail.com>
2016-08-22 08:37:51 +0800

    Add header, order, inventory components

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,61 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 /*
- *StorePicker
+ * App
+ */
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+});
+
+/*
+ * Header
+ * <Header />
+ */
+var Header = React.createClass({
+  render: function () {
+    return (
+      <p>Header</p>
+    )
+  }
+});
+
+/*
+ *  Order
+ *  <Order />
+ */
+var Order = React.createClass({
+  render: function () {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*
+ * Inventory
+ * <Inventory />
+ */
+var Inventory = React.createClass({
+  render: function () {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
+/*
+ * StorePicker
  */
 
 var StorePicker = React.createClass({
@@ -18,6 +72,6 @@ var StorePicker = React.createClass({
 });
 
 ReactDOM.render(
-  <StorePicker />,
+  <App />,
   document.getElementById('main')
 );
