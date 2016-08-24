bb41a4187f42320e976fa8d44eba967ea6fbc414
monsteronfire <monsteronfire@gmail.com>
2016-08-24 08:39:26 +0800

    Add fun name helper as defaultValue of storepicker input

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
 var browserHistory = ReactRouter.browserHistory;
+
+var helpers = require('./helpers');
 /*
  * App
  */
@@ -81,7 +83,7 @@ var StorePicker = React.createClass({
     return (
       <form className="store-selector">
         <h2>Please enter a store</h2>
-        <input type="text" ref="storeId" required />
+        <input type="text" ref="storeId" defaultValue={helpers.getFunName()}  required />
         <input type="submit" />
       </form>
     )
